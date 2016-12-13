27b1e25b94fdeae23506a0ef82cb786947481d1d
ElizavetaKonovalova <lisakonovalova@gmail.com>
2016-12-13 17:00:13 +1000

    - Changed properties for creating Client Structural Maps.

--- src/main/java/application/controllers/ClientStructuralMapController.java
+++ src/main/java/application/controllers/ClientStructuralMapController.java
@@ -1,5 +1,6 @@
 package application.controllers;
 
+import application.models.ClientsStructuralMaps;
 import application.repositories.ClientStructuralMapsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,15 +16,21 @@ public class ClientStructuralMapController {
 
     @RequestMapping(value = "create", produces = "application/json")
     public void create(@RequestParam("cohort") String cohort, @RequestParam("loc") String location, @RequestParam("niche") String niche,
-                         @RequestParam("mtxo") String matrixone, @RequestParam("mtxtw") String matrixtwo,
-                         @RequestParam("mtxth") String matrixthree, @RequestParam("mtxfo") String matrixfour,
-                         @RequestParam("mtxfi") String matrixfive, @RequestParam("wun") String wu_name, @RequestParam("wuid") String wu_id,
-                         @RequestParam("sector") String sector, @RequestParam("wulvlfi") String wu_level_five,
-                         @RequestParam("wulvlfo") String wu_level_four, @RequestParam("wulvlo") String wu_level_one,
-                         @RequestParam("wulvlth") String wu_level_three, @RequestParam("wulvltw") String wu_level_two,
-                         @RequestParam("wulvlz") String wu_level_zero, @RequestParam("cid") String client_id) throws Exception {
+                       @RequestParam("m1") String matrixone, @RequestParam("m2") String matrixtwo,
+                       @RequestParam("m3") String matrixthree, @RequestParam("m4") String matrixfour,
+                       @RequestParam("m5") String matrixfive, @RequestParam("wun") String wu_name, @RequestParam("wuid") String wu_id,
+                       @RequestParam("sector") String sector, @RequestParam("lvl0") String wu_level_zero,
+                       @RequestParam("lvl1") String wu_level_one, @RequestParam("lvl2") String wu_level_two,
+                       @RequestParam("lvl3") String wu_level_three, @RequestParam("lvl4") String wu_level_four,
+                       @RequestParam("lvl5") String wu_level_five, @RequestParam("cid") String client_id) throws Exception {
+
         this.clientStructuralMapsRepository.create(cohort, location, niche, matrixone, matrixtwo, matrixthree,
                 matrixfour, matrixfive, wu_name, wu_id, sector, wu_level_five, wu_level_four, wu_level_one, wu_level_three,
                 wu_level_two, wu_level_zero, client_id);
+    }
+
+    @RequestMapping(value = "gwuid", produces = "application/json")
+    public ClientsStructuralMaps getByWUID(@RequestParam("id") String wu_id) {
+        return this.clientStructuralMapsRepository.getWorkUnitByWUID(wu_id);
     }
 }
