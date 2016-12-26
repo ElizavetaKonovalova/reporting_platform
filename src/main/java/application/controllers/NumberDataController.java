984c35b57b2368706a3248a18188f61fe3986612
ElizavetaKonovalova <lisakonovalova@gmail.com>
2016-12-26 17:49:05 +1000

    - Change a bit mapping names.

--- src/main/java/application/controllers/NumberDataController.java
+++ src/main/java/application/controllers/NumberDataController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("nd")
+@RequestMapping("numberdata")
 public class NumberDataController {
 
     @Autowired
--- src/main/java/application/controllers/TextDataController.java
+++ src/main/java/application/controllers/TextDataController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("td")
+@RequestMapping("textdata")
 public class TextDataController {
 
     @Autowired
